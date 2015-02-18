a59833eb0b3752882548db719eb9bbc1d07c3527
zilin <eoline@163.com>
2015-02-17 19:20:05 -0600

    more for pacing

--- quic/quic_connection.cc
+++ quic/quic_connection.cc
@@ -6,6 +6,7 @@
 
 #include <string.h>
 #include <sys/types.h>
+#include <stdio.h>
 
 #include <algorithm>
 #include <iterator>
@@ -1377,6 +1378,7 @@ bool QuicConnection::CanWrite(HasRetransmittableData retransmittable) {
 
   // If the scheduler requires a delay, then we can not send this packet now.
   if (!delay.IsZero()) {
+    printf("delay: %lu\n", delay.ToMilliseconds());
     send_alarm_->Update(now.Add(delay), QuicTime::Delta::FromMilliseconds(1));
     DVLOG(1) << ENDPOINT << "Delaying sending " << delay.ToMilliseconds()
              << "ms";
